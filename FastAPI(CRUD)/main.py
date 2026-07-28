86816a04ffcf916c8a5fc7a5560cd4077834ffca
Ahmad Junaid <fa23-bse-019@cuilahore.edu.pk>
2026-07-28 23:48:45 +0500

    Stage 3: create with validation

--- FastAPI(CRUD)/main.py
+++ FastAPI(CRUD)/main.py
@@ -1,6 +1,39 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 app=FastAPI()
+
+class TaskCreate(BaseModel):
+    title : str
+
+# FastAPI exception Handling
+@app.exception_handler(RequestValidationError)
+async def exception_validator(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=400,
+        content={
+            "Details": "Invalid Request",
+            "Error": exc.errors()
+        }
+    )
+
+@app.post("/tasks",status_code=201)
+def create_task(task:TaskCreate):
+    if task.title.strip()=="":
+#  Exceptions Handling Manually
+        raise HTTPException(
+            status_code=400,
+            detail="Title cannot be empty"
+        )
+    new_task={
+        "id" : len(tasks)+1,
+        "title" : task.title,
+        "done" : False
+    }
+    tasks.append(new_task)
+    return new_task
 
 @app.get("/")
 def root():
