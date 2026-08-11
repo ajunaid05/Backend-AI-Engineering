1e317125e9e63fb41e60458385412887ed126404
Ahmad Junaid <fa23-bse-019@cuilahore.edu.pk>
2026-08-12 01:15:53 +0500

    Stage 0: setup server and Supabase client

---
+++ A4_Auth_Login_Protect/main.py
@@ -0,0 +1,31 @@
+import os
+
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from supabase import create_client,Client
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise RuntimeError("SUPABASE_URL and SUPABASE_KEY must be set in .env")
+
+supabase:Client = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY
+)
+
+app = FastAPI(
+    title= "Auth login and protected API",
+    description= "FastAPI authentication API using Supabase Auth",
+    version = "1.0.0"
+)
+
+@app.get("/")
+def root():
+    return {
+        "message" : "Auth API is running.",
+        "Status" : "Supabase Connected"
+    }
